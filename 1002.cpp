baf5df288e097f12d818a55c7d262203eb1617f6
PBS <sortsimilar@gmail.com>
2018-04-16 00:09:18 -0400

    用class表示了多项式加法

--- 1002.cpp
+++ 1002.cpp
@@ -3,69 +3,51 @@
 
 using namespace std;
 
-int main()
+class Polynomial
 {
-	int first_num;
-	cin >> first_num;
-	vector<int> first_exponent(first_num);
-	vector<float> first_coefficient(first_num);
-
-	for (int i = 0;i < first_num;i++)
-	{
-		cin >> first_exponent[i];
-		cin >> first_coefficient[i];
-	}
-	
-	int second_num; // number of second polynomial;
-	cin >> second_num;
-	vector<int> second_exponent(second_num);
-	vector<float> second_coefficient(second_num);
-
-	for (int i = 0;i < second_num;i++)
-	{
-		cin >> second_exponent[i];
-		cin >> second_coefficient[i];
-	}
-
-	int output_num = 0;
-	vector<int> output_exponent;
-	vector<float> output_coefficient;
+public:
+	int num;
+	vector<int> exponent;
+	vector<float> coefficient;
+};
 
 
+Polynomial add_polynomial(Polynomial first, Polynomial second)
+{
+	Polynomial output;
 	int first_index = 0;
 	int second_index = 0;
 
-	first_exponent.push_back(-1);
-	second_exponent.push_back(-1);
+	first.exponent.push_back(-1);
+	second.exponent.push_back(-1);
 
-//	for (int i = 0;i < first_num;i++) cout << first_exponent[i] << " ";
 	while (1)
 	{
-		if ((first_exponent[first_index] == -1) && (second_exponent[second_index] == -1))
+		if ((first.exponent[first_index] == -1) && (second.exponent[second_index] == -1))
 		{
 			break;
 		}
 		else
 		{
-			if (first_exponent[first_index] < second_exponent[second_index])
+			if (first.exponent[first_index] < second.exponent[second_index])
 			{
-				output_exponent.push_back(second_exponent[second_index]);
-				output_coefficient.push_back(second_coefficient[second_index]);
+				output.exponent.push_back(second.exponent[second_index]);
+				output.coefficient.push_back(second.coefficient[second_index]);
 				second_index++;
 			}
-			else if (first_exponent[first_index] > second_exponent[second_index])
+			else if (first.exponent[first_index] > second.exponent[second_index])
 			{
-				output_exponent.push_back(first_exponent[first_index]);
-				output_coefficient.push_back(first_coefficient[first_index]);
+				output.exponent.push_back(first.exponent[first_index]);
+				output.coefficient.push_back(first.coefficient[first_index]);
 				first_index++;
 			}
 			else
 			{
-				float sum = first_coefficient[first_index] + second_coefficient[second_index];
+				float sum = first.coefficient[first_index] + second.coefficient[second_index];
 				if (sum != 0)
 				{
-					output_exponent.push_back(first_exponent[first_index]);
-					output_coefficient.push_back(sum);
+					output.exponent.push_back(first.exponent[first_index]);
+					output.coefficient.push_back(sum);
 				}
 				first_index++;
 				second_index++;
@@ -75,15 +57,50 @@ int main()
 
 	}
 
-	cout << output_exponent.size();
-	if (output_exponent.size() > 0)
+	return output;
+}
+
+
+int main()
+{
+	Polynomial first, second, output;
+
+	cin >> first.num;
+
+	for (int i = 0;i < first.num;i++)
+	{
+		int temp_exponent;
+		cin >> temp_exponent;
+		first.exponent.push_back(temp_exponent);
+		float temp_coefficient;
+		cin >> temp_coefficient;
+		first.coefficient.push_back(temp_coefficient);
+	}
+	
+	cin >> second.num;
+
+	for (int i = 0;i < second.num;i++)
+	{
+		int temp_exponent;
+		cin >> temp_exponent;
+		second.exponent.push_back(temp_exponent);
+		float temp_coefficient;
+		cin >> temp_coefficient;
+		second.coefficient.push_back(temp_coefficient);
+	}
+
+
+	output = add_polynomial(first, second);
+
+	cout << output.exponent.size();
+	if (output.exponent.size() > 0)
 	{
 		cout << " ";
-		for (int i = 0;i < output_exponent.size();i++)
+		for (int i = 0;i < output.exponent.size();i++)
 		{
-			cout << output_exponent[i] << " ";
-			printf("%.1f", output_coefficient[i]);
-			if (i != output_exponent.size() - 1) cout << " ";
+			cout << output.exponent[i] << " ";
+			printf("%.1f", output.coefficient[i]);
+			if (i != output.exponent.size() - 1) cout << " ";
 		}
 	}
 
