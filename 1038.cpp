7f6513661e13edba49edb8e414735a93fcad2127
PBS <sortsimilar@gmail.com>
2018-06-08 17:17:09 +0800

    Recover the Smallest Number (30)
    不用进行数转换， 直接比较字符串就行。

--- 1038.cpp
+++ 1038.cpp
@@ -5,21 +5,10 @@
 #include<vector>
 using namespace std;
 
-// convert from string to long long int;
-long long string_to_int(string target)
-{
-	stringstream ss;
-	ss<<target;
-	long long result;
-	ss>>result;
-
-	return result;
-}
-
 
 bool compare(string first, string second)
 {
-	return string_to_int(first+second) <= string_to_int(second+first);
+	return (first+second) <= (second+first);
 }
 
 
