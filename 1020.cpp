122f467fa9915d76f832a82bc780bb3342549282
PBS <sortsimilar@gmail.com>
2018-06-26 16:56:56 +0800

    Tree Traversals (25)

--- 1020.cpp
+++ 1020.cpp
@@ -1,131 +1,108 @@
 #include<iostream>
-#include<queue>
-#include<vector>
-using namespace std;
-
-
+#include<queue>  
+#include<vector>  
+using namespace std;  
+      
 struct Node
-{
-	int key;
-	int left;
-	int right;	
-	int parent;
-};
+{  
+    int data;  
+    Node* left;  
+    Node* right;  
+};  
 
 vector<int> post_order;
 vector<int> in_order;
-vector<Node> binary;
-vector<Node> level_sequence;
 
-
-
-int build_tree(int inStart, int inEnd, int postStart, int postEnd)
-{
-	if (inStart > inEnd || postStart > postEnd)
-	{
-		return -1;
-	}
-
-	int root = post_order[postEnd];
-
-	int k = 0;
-	for (int i = inStart; i <= inEnd; i++) 
-	{
-		if (in_order[i] == root) 
-		{
-			k = i;
-			break;
-		}
-	}
-
-	binary[root].left = build_tree(inStart, k - 1, postStart, postStart + k - (inStart + 1));
-	// Becuase k is not the length, it it need to -(inStart+1) to get the length
-	binary[root].right = build_tree(k + 1, inEnd, postStart + k- inStart, postEnd - 1);
-	// postStart+k-inStart = postStart+k-(inStart+1) +1
-
-
-	return root;
-}
-
-
-// print level
-void level_order(int root,int totalNode)
+vector<int> result;  
+    
+Node* build_tree(int postStart, int postEnd, int midStart, int midEnd)
+{  
+    if (postStart > postEnd)
+	{  
+        return NULL;  
+    }  
+  
+    Node * root = new Node;  
+    //查找后序队列中，最后一个数据在中序队列中的位置  
+    int i;  
+    for (i = midStart; i <= midEnd; i++)
+	{  
+        if (in_order[i] == post_order[postEnd])
+		{  
+            //找到了，中序队列中第i个位置就是的  
+            break;  
+        }  
+      
+    }  
+    //中序队列中距离的起始位置的个数，即第i个位置为根结点，左边num个是它的左子树的个数  
+    int numStart = i - midStart;  
+    root->data = post_order[postEnd];  
+    root->left = build_tree(postStart, postStart + numStart - 1, midStart , i - 1);   
+    root->right = build_tree(postStart + numStart, postEnd - 1, i + 1, midEnd);  
+    return root;  
+}  
+      
+      
+      
+//层次遍历二叉树  
+void level_order(Node * root,int totalNode)
 {  
     int num = 0;  
-    if (root == -1)
+    if (root == NULL)
 	{  
         return;  
     }  
-    queue<int> q;  
+    queue<Node*> q;  
     q.push(root);  
     while (!q.empty())
 	{  
         //出队，然后访问该数据  
-        int current = q.front();  
-        q.pop();
-  
-        cout<<binary[current].key+1;
-  
-        num++;  
-        if (num < totalNode)    cout<<" "; 
- 
-        if (binary[current].left != -1)
-		{  
-            q.push(binary[current].left);//左子树入队  
-        }
-  
-        if (binary[current].right != -1)
-		{  
-            q.push(binary[current].right);//右子树入队  
-        }  
-    }  
+        Node * current = q.front();  
+        q.pop();  
 
+		result.push_back(current->data); 
+        num++;  
+  
+        if (current->left != NULL)    q.push(current->left);//左子树入队    
+        if (current->right != NULL)   q.push(current->right);//右子树入队  
+    }
+	  
     return ;  
+}  
+      
+int main()
+{  
+	int N;
+	cin>>N;  
+
+    //后序序列  
+    for (int i = 0; i<N; i++)
+	{  
+		int temp;
+		cin>>temp;
+		post_order.push_back(temp);
+    }  
+    //中序序列  
+    for (int i = 0; i<N; i++)
+	{  
+		int temp;
+		cin>>temp;
+		in_order.push_back(temp);
+    }  
+    //根据后序和中序序列建二叉树  
+    Node* root = build_tree(0, N-1, 0, N-1);  
+    //层次遍历二叉树  
+    level_order(root, N);  
+
+	for(int i=0;i<N;i++)
+	{
+		cout<<result[i];
+		if(i!=N-1)    cout<<" ";
+	}
+  
+    return 0;  
 }  
 
 
-int main()
-{
-// input process ////////////////////////////////////////
-	int N;
-	cin>>N;
 
-
-	for(int i=0;i<N;i++)
-	{
-		int temp;
-		cin>>temp;
-		post_order.push_back(temp-1);
-	}
-
-	for(int i=0;i<N;i++)
-	{
-		int temp;
-		cin>>temp;
-		in_order.push_back(temp-1);
-	}
-
-// initialize binary tree;
-	for(int i=0;i<N;i++)
-	{
-		Node temp;
-		temp.key = i;
-		temp.parent = -1;
-		binary.push_back(temp);
-	}
-
-
-	int root = build_tree(0, N-1, 0, N-1);
-
-
-	level_order(root, N);
-
-	return 0;
-}
-
-/*
-7
-2 3 1 5 7 6 4
-1 2 3 4 5 6 7
-*/
 
