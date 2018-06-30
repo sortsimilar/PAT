8d4d2036123f03e599f05c3148a84aef5d853b4f
PBS <sortsimilar@gmail.com>
2018-06-30 11:07:44 +0800

    Highest Price in Supply Chain (25)

--- 1090.cpp
+++ 1090.cpp
@@ -6,26 +6,41 @@ using namespace std;
 struct Node
 {
 	int key;
-	int parent;
+	vector<int> child;
+
 	int height;	// distance between node and root;
+	
+	double data;
 };
 
+
+double P; // unit price given by the root supplier;
+double r; // percentage rate of price increment for each distributor or retailer;
 vector<Node> tree;
 
-int get_height(int index)
+int max_level = -1;
+double max_price;
+
+void tree_dfs(int current)
 {
-	int height = 0;
-	int current = index;
-	while(current != -1)
+//	cout<<tree[current].key<<" "<<tree[current].height<<endl;
+
+	if(tree[current].child.size() == 0)
 	{
-		current = tree[current].parent;
-		height++;
+		if(tree[current].height>max_level)
+		{
+			max_level = tree[current].height;
+			max_price = tree[current].data;
+		}
 	}
 
-	return height-1;
+	for(int i=0;i<tree[current].child.size();i++) // childs 存放指向其每個子結點的指標
+	{ 
+		tree[tree[current].child[i]].height = tree[current].height + 1;
+		tree[tree[current].child[i]].data = tree[current].data * (1.0 + r/100.0);
+		tree_dfs(tree[current].child[i]);   
+    }	
 }
-
-
 
 
 
@@ -33,9 +48,9 @@ int main()
 {
 	int N; // total number of the members in the supply chain;
 	cin>>N;
-	double P; // price given by the root supplier; 
+	
 	cin>>P;
-	double r; // percentage rate of price increment for each distributor or retailer;
+	
 	cin>>r;
 
 	// initialize tree node;
@@ -43,46 +58,80 @@ int main()
 	{
 		Node temp;
 		temp.key = i;
-		cin>>temp.parent;
 		temp.height = 0;
+
 		tree.push_back(temp);
 	}
+	
 
-	int max_height = -1;
+	int root;
 	for(int i=0;i<N;i++)
 	{
-		tree[i].height = get_height(i);
-		if(tree[i].height >= max_height)
+		int temp;
+		cin>>temp;
+		if(temp!=-1)
 		{
-			max_height = tree[i].height;
+			tree[temp].child.push_back(i);
+		}
+		else
+		{
+			root = i;
 		}
 	}
+
+	tree[root].data = P;
+
+
+/*
+	for(int i=0;i<N;i++)
+	{
+		cout<<i<<" ";
+		for(int j=0;j<tree[i].child.size();j++)
+		{
+			cout<<tree[i].child[j]<<" ";
+		}
+		cout<<endl;
+	}
+*/
+
+
+	tree_dfs(root);
+
+
+	printf("%.2f", max_price);
+
+
 
 	int counter = 0;
 	for(int i=0;i<N;i++)
 	{
-//		cout<<tree[i].key<<" "<<tree[i].parent<<" "<<tree[i].height<<endl;
-		if(tree[i].height == max_height)
+//		cout<<tree[i].height<<" "<<tree[i].data<<endl;
+		if((tree[i].height==max_level)&&(tree[i].child.size()==0))
 		{
 			counter++;
 		}
 	}
 
-	double sum = P;
-	for(int i=0;i<max_height;i++)
-	{
-		sum = sum * (1.0 + r/100.0);
-	}
-
-	printf("%.2f", sum);
+		
 	cout<<" "<<counter;
+
 
 
 	return 0;
 }
 
 
-
-
-
+/*
+10 1.80 1.00
+3 2 3 5
+1 9
+1 4
+1 7
+0 7
+2 6 1
+1 8
+0 9
+0 4
+0 3
+*/
 
