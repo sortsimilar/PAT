6c42ab3ab3573d2b702c9a046fc3d5038eeaf294
PBS <sortsimilar@gmail.com>
2018-08-05 22:47:51 +0800

    Complete Binary Tree（25 point(s)）

--- 1110.cpp
+++ 1110.cpp
@@ -1,5 +1,3 @@
-// level order of binary tree;
-
 #include<iostream>
 #include<sstream>
 #include<string>
@@ -9,16 +7,16 @@ using namespace std;
 // type of node;
 struct Node
 {
-	int name;
+	int key;
 	int left;
 	int right;
-	int parent;
-	int position;
 };
 
 
+int counter = 0;
+int last_node = -1;
 vector<Node> binary;
-vector<Node> level_sequence;
+vector<int> level_sequence;
 
 
 int string_to_int(string temp)
@@ -32,16 +30,44 @@ int string_to_int(string temp)
 }
 
 
-// print level
-void level_order(Node root, int level)
-{
-	if(level == 1)    level_sequence.push_back(root);
-	else if(level > 1)
+void level_order(int root)
+{    
+
+	vector<int> q;  
+    q.push_back(root);  
+
+
+    while (!q.empty())
 	{
-		if(root.left != -1)    level_order(binary[root.left], level-1); 
-		if(root.right != -1)    level_order(binary[root.right], level-1);
-	}
-}
+		int current = q[0];
+		q.erase(q.begin());
+		
+//		level_sequence.push_back(binary[current].key); 
+
+  
+        if (current != -1)    
+		{
+			q.push_back(binary[current].left);   
+        	q.push_back(binary[current].right); 
+			counter++;
+			last_node = current;
+		}
+		else
+		{
+			if(counter==binary.size())
+			{
+				cout<<"YES "<<last_node;
+			}
+			else
+			{
+				cout<<"NO "<<root;
+			}
+
+			break;
+		}
+    }
+	  
+} 
 
 
 
@@ -50,97 +76,52 @@ void level_order(Node root, int level)
 int main()
 {
 	
-	int N;
+	unsigned N;
 	cin>>N;
 
 	
 	// build binary tree;
-	for(int i=0;i<N;i++)
+	vector<bool> hash_table(N, false);
+
+	for(unsigned i=0;i<N;i++)
 	{
 		Node temp;
-		temp.name = i;
+		temp.key = i;
 		string left; 
 		string right;
 		cin>>left;
 		cin>>right;		
-		if(left != "-")    temp.left = string_to_int(left);
+		if(left != "-")
+		{
+			temp.left = string_to_int(left);
+			hash_table[temp.left] = true;
+		}
 		else    temp.left = -1;
-		if(right != "-")    temp.right = string_to_int(right);
-		else    temp.right = -1;
 
-		temp.parent = -1;
+		if(right != "-")
+		{
+			temp.right = string_to_int(right);
+			hash_table[temp.right] = true;
+		}
+		else    temp.right = -1;
 
 		binary.push_back(temp);
 	}
 
-	// set parent for every node;
-	for(int i=0;i<N;i++)
-	{
-		int left = binary[i].left;
-		int right = binary[i].right;
-
-		if(left != -1)    binary[left].parent = i;
-		if(right != -1)    binary[right].parent = i;
-
-	}
-
-	// find height of binary tree
+	// find root of binary tree;
 	int root = -1;
-	int height = 0;	
-	for(int i=0;i<N;i++)
+	for(unsigned i=0;i<hash_table.size();i++)
 	{
-		int current_node = i;
-		int current_height = 0;
-		while(current_node != -1)
+		if(hash_table[i]==false)
 		{
-			current_height++;
-			root = current_node;
-			current_node = binary[current_node].parent;
-		}
-
-		if(height<current_height)    height = current_height;
-	}
-
-	// get level order for each level;
-	for(int i=1;i<=height;i++)
-	{
-		level_order(binary[root], i);
-	}
-
-	// output result of level order;
-	for(int i=0;i<level_sequence.size();i++)
-	{
-		int temp = level_sequence[i].name;
-		binary[temp].position = i;
-	}
-
-
-	bool flag_complete = true;
-
-
-	for(int i=0;i<N;i++)
-	{
-		if(i != root)
-		{
-//			cout<<binary[i].position<<" "<<binary[binary[i].parent].position<<endl;
-			int parent_lilun = (binary[i].position - 1) / 2;
-			if(parent_lilun != binary[binary[i].parent].position)
-			{
-				flag_complete = false;
-				break;
-			}
+			root = i;
+			break;
 		}
 	}
 
 
-	if(flag_complete==true)
-	{
-		cout<<"YES"<<" "<<level_sequence[N-1].name;
-	}
-	else
-	{
-		cout<<"NO"<<" "<<root;
-	}
+	level_order(root);
+
 
 
 
