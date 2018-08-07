79ab451029a94a57ad7e5e287da907b4393124ee
PBS <sortsimilar@gmail.com>
2018-08-07 15:25:16 +0800

    Linked List Sorting (25)

--- 1052.cpp
+++ 1052.cpp
@@ -1,42 +1,22 @@
 #include<algorithm>
-#include<iostream>
-#include<sstream>
+#include<stdio.h>
 #include<string>
 #include<vector>
 using namespace std;
 
 struct Node
 {
-	int prev;
 	int addr;
 	int key;
 	int next;
 
-	bool flag;
 };
 
 
 
-vector<Node> linklist(100001);
+vector<Node> linklist(200001);
 vector<Node> linklist_origin;
-vector<Node> save;
 
-
-string int_to_string(int addr)
-{
-	string result;
-	stringstream ss;
-	ss<<addr;
-	ss>>result;
-
-
-	if(result != "-1")
-	{
-		while(result.length() < 5)    result = "0" + result;
-	}
-
-	return result;
-}
 
 // create link list;
 void create_list(int head)
@@ -44,12 +24,7 @@ void create_list(int head)
 	int	current = head;
 	while(current != -1)
 	{	
-		Node temp;
-		temp.addr = linklist[current].addr;
-		temp.key = linklist[current].key;
-		temp.next = linklist[current].next;
-		linklist_origin.push_back(temp);
-
+		linklist_origin.push_back(linklist[current]);
 		current = linklist[current].next;
 	}
 }
@@ -65,58 +40,55 @@ bool compare(Node first, Node second)
 int main()
 {
 	int N; // total number of nodes;
-	cin>>N; 
+	scanf("%d", &N);
 	int head;
-	cin>>head;
+	scanf("%d", &head);
 
-	
-	for(int i=0;i<N;i++)
-	{
-		Node temp;
-		cin>>temp.addr;
-		cin>>temp.key;
-		cin>>temp.next;
-		temp.flag = false;
-
-		save.push_back(temp);
-	}
 
 	// initialize link list;	
 	for(int i=0;i<linklist.size();i++)
-	{
+	{		
 		linklist[i].addr = -1;
 		linklist[i].next = -1;
 	}
 
-	// allocate link list nodes;
 	for(int i=0;i<N;i++)
 	{
-		int temp = save[i].addr;
-		linklist[temp].addr = save[i].addr;
-		linklist[temp].key = save[i].key;
-		linklist[temp].next = save[i].next;
+		int temp;;
+		scanf("%d", &temp);
+		linklist[temp].addr = temp;
+		scanf("%d", &linklist[temp].key);
+		scanf("%d", &linklist[temp].next);
 	}
+
 
 	//	print link list;
 	create_list(head);
 
-
-	sort(linklist_origin.begin(), linklist_origin.end(), compare);
-
-	cout<<linklist_origin.size()<<" "<<linklist_origin[0].addr<<endl;
-	for(int i=0;i<linklist_origin.size();i++)
+	if(linklist_origin.size()>0)
 	{
-		cout<<int_to_string(linklist_origin[i].addr)<<" "<<linklist_origin[i].key<<" ";
+		sort(linklist_origin.begin(), linklist_origin.end(), compare);
 
-		if(i != linklist_origin.size()-1)
+		printf("%d %05d\n", linklist_origin.size(), linklist_origin[0].addr);
+		for(int i=0;i<linklist_origin.size();i++)
 		{
-			cout<<int_to_string(linklist_origin[i+1].addr)<<endl;
-		}
-		else
-		{
-			cout<<"-1";
+			printf("%05d %d ", linklist_origin[i].addr, linklist_origin[i].key);
+
+			if(i != linklist_origin.size()-1)
+			{
+				printf("%05d\n", linklist_origin[i+1].addr);
+			}
+			else
+			{
+				printf("-1");
+			}
 		}
 	}
+	else
+	{
+		printf("0 -1");
+	}
+
 
 	return 0;
 }
