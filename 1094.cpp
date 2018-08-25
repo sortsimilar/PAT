8f9edc4aba9e2d0888abcf339bca8e9f8a304f59
PBS <sortsimilar@gmail.com>
2018-08-25 21:58:16 +0800

    The Largest Generation（25 point(s)）

--- 1094.cpp
+++ 1094.cpp
@@ -36,13 +36,12 @@ int main()
 	cin>>M;
 
 	// initialize tree node;
+	tree.resize(N+1);
 	for(int i=0;i<N+1;i++)
 	{
-		Node temp;
-		temp.key = i;
-		temp.parent = -1;
-		temp.height = 0;
-		tree.push_back(temp);
+		tree[i].key = i;
+		tree[i].parent = -1;
+		tree[i].height = 0;
 	}
 
 	for(int i=0;i<M;i++)
@@ -93,9 +92,23 @@ int main()
 }
 
 
+/*
+
+23 13
+21 1 23
+01 4 03 02 04 05
+03 3 06 07 08
+06 2 12 13
+13 1 21
+08 2 15 16
+02 2 09 10
+11 2 19 20
+17 1 22
+05 1 11
+07 1 14
+09 1 17
+10 1 18
 
 
-
-
-
+*/
 
