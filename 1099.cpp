7ccac2a8c6bc8abc249fd25d5eb1b30f109c7ff5
PBS <sortsimilar@gmail.com>
2018-08-25 23:19:40 +0800

    Build A Binary Search Tree（30 point(s)）

--- 1099.cpp
+++ 1099.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-struct node
+struct Node
 {
 	int left;
 	int right;
@@ -13,17 +13,45 @@ struct node
 };
 
 vector<int> sequence_name;
-vector<node> binary;
+vector<Node> binary;
+vector<int> level_sequence;
 
-void inorder(node current)
+
+void in_order(Node current)
 {
 	if((current.name==-1))    return;	
 
-	if(current.left!=-1)    inorder(binary[current.left]);
+	if(current.left!=-1)    in_order(binary[current.left]);
 //	cout<<current.name<<" ";
 	sequence_name.push_back(current.name);
-	if(current.right!=-1)    inorder(binary[current.right]);
+	if(current.right!=-1)    in_order(binary[current.right]);
 }
+
+
+void level_order(int root)
+{
+	vector<int> q;
+	q.push_back(root);
+
+	while(!q.empty())
+	{
+		int current = q[0];
+		q.erase(q.begin());
+		level_sequence.push_back(binary[current].value);
+
+		if(binary[current].left != -1)    q.push_back(binary[current].left);
+		if(binary[current].right != -1)    q.push_back(binary[current].right);
+	}
+
+}
+
+
+bool compare(int a, int b)
+{
+	return a < b;
+}
+
+
 
 
 
@@ -34,29 +62,25 @@ int main()
 	cin>>N;
 
 	
-	vector<int> sequence_value(N);
-	for(int i=0;i<N;i++)
+	binary.resize(N);
+	for(int i=0;i<binary.size();i++)
 	{
-		node temp;
-		cin>>temp.left;
-		cin>>temp.right;
-		temp.name = i;
-		binary.push_back(temp);
+		cin>>binary[i].left;
+		cin>>binary[i].right;
+		binary[i].name = i;
 	}
 
+	vector<int> sequence_value(N);
 	for(int i=0;i<N;i++)
 	{
 		cin>>sequence_value[i];
 	}
 
+
 	// sort sequence value;
-	sort(sequence_value.begin(), sequence_value.end());
+	sort(sequence_value.begin(), sequence_value.end(), compare);
 
-	// get sequence of binary search tree;
-	inorder(binary[0]);
-
-
-//	for(int i=0;i<sequence_name.size();i++)    cout<<sequence_name[i]<<" ";
+	in_order(binary[0]);
 
 	// give value to name;
 	for(int i=0;i<N;i++)
@@ -65,49 +89,37 @@ int main()
 		binary[temp].value = sequence_value[i];
 	}
 
-//	for(int i=0;i<N;i++)    cout<<binary[i].value<<" "; // output value according to node number;
-	// output level order traversal;
-	vector<int> current_layer;
-	vector<int> next_layer;
-	vector<int> level_order;
-	current_layer.push_back(0);
-	level_order.push_back(0);
-	if(binary[0].left != -1)    next_layer.push_back(binary[0].left);
-	if(binary[0].right != -1)    next_layer.push_back(binary[0].right);
 
-	while(next_layer.size() != 0)
+	level_order(0);
+
+	for(int i=0;i<level_sequence.size();i++)
 	{
-		current_layer.clear();
-		for(int i=0;i<next_layer.size();i++)
-		{
-			current_layer.push_back(next_layer[i]);
-			level_order.push_back(next_layer[i]);
-		}
+		cout<<level_sequence[i];
 
-		next_layer.clear();
-		for(int i=0;i<current_layer.size();i++)
-		{
-			int temp = current_layer[i];
-			if(binary[temp].left != -1)    next_layer.push_back(binary[temp].left);
-			if(binary[temp].right != -1)    next_layer.push_back(binary[temp].right);
-		}
+		if(i!=level_sequence.size()-1)    cout<<" ";
 	}
 
-	for(int i=0;i<N;i++)    
-	{
-		cout<<binary[level_order[i]].value;
-		if(i != N-1)    cout<<" ";
-	}
 
 	return 0;
 }
 
 
+/*
+
+9
+1 6
+2 3
+-1 -1
+-1 4
+5 -1
+-1 -1
+7 -1
+-1 8
+-1 -1
+73 45 11 58 82 25 67 38 42
 
 
-
-
-
+*/
 
 
 
