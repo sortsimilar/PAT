206a09956214898500e355da1fdd019b66338883
PBS <sortsimilar@gmail.com>
2018-08-29 23:26:17 +0800

    Birds in Forest（25 point(s)）

--- 1118.cpp
+++ 1118.cpp
@@ -1,188 +1,87 @@
 #include<iostream>
-#include<map>
 #include<string>
 #include<vector>
-
 using namespace std;
 
 
-
-struct each_pic
-{
-	vector<string> pic_member;
-};
-
-
-int counter = 0; // count num of trees;
-int idNumber = 1;
-int num_v; // number of vertices;
-vector<each_pic> pics;
-map<string, int> string_to_int;
-map<int, string> int_to_string;
-vector<bool> adjacency_matrix;
-vector<bool> visited;
 vector<string> result;
+const int maxn = 10010;
+vector<int> fa(10010, 0);
+vector<int> cnt(10010, 0);
+vector<bool> exist(10010, false);
 
-bool test_edge(int start, int end)
+int find_father(int x) 
 {
-	bool flag = false;
-
-	if(adjacency_matrix[(start-1)*num_v+(end-1)]==true)    flag = true;
-	if(adjacency_matrix[(start-1)+(end-1)*num_v]==true)    flag = true;
-
-	return flag;
+	if(fa[x] == x)    return x;
+	else    find_father(fa[x]);
 }
 
 
-// DFS one range;
-void DFS(int i)
+void Union(int a, int b) 
 {
-	visited[i-1] = true;
-//	cout<<i<<" ";
-	for(int j=1;j<=num_v;j++)
+    int RootA = find_father(a);
+    int RootB = find_father(b);
+
+    if(RootA != RootB)    fa[RootA] = RootB;
+}
+
+
+
+int main() 
+{
+	int n, m, k;
+    cin>>n;
+    for(int i = 1; i <= maxn; i++)
 	{
-		if((test_edge(i, j)==true)&&(visited[j-1]==false))
+        fa[i] = i;
+	}
+
+    int id, temp;
+    for(int i = 0; i < n; i++) 
+	{
+		cin>>k>>id;
+        exist[id] = true;
+        for(int j = 0; j < k-1; j++) 
 		{
-			DFS(j);
-		}
-	}
-
-}
-
-// DFS all map;
-void DFSTraverse()
-{
-	// initialize visited matrix;
-	for(int i=1;i<=num_v;i++)
-	{
-		visited[i-1] = false;
-	}
-
-
-	for(int i=1;i<=num_v;i++)
-	{
-		if(visited[i-1]==false)
-		{
-			DFS(i);
-			counter++;
-		}
-	}
-}
-
-// test connected graph;
-bool test_connect(int start, int end)
-{
-	for(int i=0;i<num_v;i++)
-	{
-		visited[i] = false;
-	}
-
-
-	DFS(start);
-
-	if(visited[end-1]==false)    return false;
-	else    return true;
-	
-}
-
-
-
-
-int trans_func(string s) 
-{
-    if(string_to_int[s] == 0) 
-	{
-        string_to_int[s] = idNumber;
-        int_to_string[idNumber] = s;
-        return idNumber++;
-    } 
-	else 
-	{
-        return string_to_int[s];
+			cin>>temp;
+            Union(id, temp);
+            exist[temp] = true;
+        }
     }
-}
 
-
-
-int main()
-{
-	int N; // number of pictures;
-	cin>>N;
-
-	pics.resize(N);
-	for(int i=0;i<N;i++)
+    for(int i = 1; i <= maxn; i++) 
 	{
-		int K;
-		cin>>K;
-
-		pics[i].pic_member.resize(K);
-		for(int j=0;j<K;j++)
+        if(exist[i] == true) 
 		{
-			cin>>pics[i].pic_member[j];
-			trans_func(pics[i].pic_member[j]);
-		}
-	}
+            int root = find_father(i);
+            cnt[root]++;
+        }
+    }
 
-	num_v = string_to_int.size();
-
-// create adjacency matrix;
-	for(int i=0;i<num_v*num_v;i++)
+    int numTrees = 0, numBirds = 0;
+    for(int i = 1; i <= maxn; i++) 
 	{
-		adjacency_matrix.push_back(false);
-	}
-
-
-	for(int i=0;i<pics.size();i++)
-	{
-
-		if(pics[i].pic_member.size()>1)
+        if(exist[i] == true && cnt[i] != 0) 
 		{
-			for(int j=0;j<pics[i].pic_member.size()-1;j++)
-			{
-				int start = string_to_int[pics[i].pic_member[j]] - 1;
-				int end = string_to_int[pics[i].pic_member[j+1]] - 1;
+            numTrees++;
+            numBirds += cnt[i];
+        }
+    }
 
-//				cout<<start<<" "<<end<<endl;
+	
+	cin>>m;
 
-				adjacency_matrix[start*num_v + end] = true; 
-				adjacency_matrix[start + end*num_v] = true; 
-			}
-		}
-	}
-
-//	cout<<string_to_int.size();
-
-	// generate visited point;
-	visited.resize(num_v);
-	for(int i=0;i<num_v;i++)
+    int ida, idb;
+    for(int i = 0; i < m; i++) 
 	{
-		visited[i] = false;
-	}
+		cin>>ida>>idb;
 
-	DFSTraverse();
-//	cout<<endl<<counter<<endl;
+		if(find_father(ida) == find_father(idb))    result.push_back("Yes");
+		else    result.push_back("No");
+    }
 
-	int Q;
-	cin>>Q;
 
-	for(int i=0;i<Q;i++)
-	{
-		string start_string;
-		cin>>start_string;
-		string end_string;
-		cin>>end_string;
-
-		int start = string_to_int[start_string];
-		int end = string_to_int[end_string];
-
-		if(test_connect(start, end)==false)    result.push_back("No");
-		else    result.push_back("Yes");
-	}
-
-	cout<<counter<<" "<<string_to_int.size()<<endl;
-
-//	cout<<test_connect(string_to_int["3"], string_to_int["7"]);
-
+	cout<<numTrees<<" "<<numBirds<<endl;
 	for(int i=0;i<result.size();i++)
 	{
 		cout<<result[i];
@@ -190,5 +89,19 @@ int main()
 	}
 
 
-	return 0;
+    return 0;
 }
+
+
+/*
+
+4
+3 10 1 2
+2 3 4
+4 1 5 7 8
+3 9 6 4
+2
+10 5
+3 7
+
+*/
