d1f131249bf198c7f5d2c812c06764d98c7233fa
PBS <sortsimilar@gmail.com>
2018-08-31 10:33:02 +0800

    Birds in Forest（25 point(s)）

--- 1118.cpp
+++ 1118.cpp
@@ -1,93 +1,67 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <cstdio>
+#include <iostream>
 using namespace std;
 
+int father[10001];
 
-vector<string> result;
-const int maxn = 10010;
-vector<int> fa(10010, 0);
-vector<int> cnt(10010, 0);
-vector<bool> exist(10010, false);
 
-int find_father(int x) 
+int find_root(int x)
 {
-	if(fa[x] == x)    return x;
-	else    find_father(fa[x]);
+	if (father[x] < 0)    return x;
+	else    return father[x] = find_root(father[x]);
 }
 
 
-void Union(int a, int b) 
+void union_set(int s1, int s2)
 {
-    int RootA = find_father(a);
-    int RootB = find_father(b);
+	int r1 = find_root(s1);
+	int r2 = find_root(s2);
 
-    if(RootA != RootB)    fa[RootA] = RootB;
+	if(r1 != r2)    father[r1] = r2;
 }
 
 
-
-int main() 
+int main()
 {
-	int n, m, k;
-    cin>>n;
-    for(int i = 1; i <= maxn; i++)
+	for(int i=0;i<10001;i++)    father[i] = -1;
+	int n, k, bi, q;
+	cin >> n;
+	int pic[10001];
+	for(int i=0;i<10001;i++)    pic[i] = 0;
+
+	int count = 0;
+	for (int i = 1; i <= n; i++) 
 	{
-        fa[i] = i;
+		cin >> k;
+		for (int j = 0; j < k; j++) 
+		{
+			cin >> bi;
+			if (pic[bi] == 0) 
+			{
+				pic[bi] = i;
+				count++;
+			}
+			else 
+			{
+				union_set(i, pic[bi]);
+			}
+		}
 	}
 
-    int id, temp;
-    for(int i = 0; i < n; i++) 
+	int nTree = 0;
+	for (int i = 1; i <= n; i++)
+		if (father[i] < 0) nTree++;
+
+
+	cout<<nTree<<" "<<count<<endl;
+	cin >> q;
+
+	for (int i = 0; i < q; i++) 
 	{
-		cin>>k>>id;
-        exist[id] = true;
-        for(int j = 0; j < k-1; j++) 
-		{
-			cin>>temp;
-            Union(id, temp);
-            exist[temp] = true;
-        }
-    }
-
-    for(int i = 1; i <= maxn; i++) 
-	{
-        if(exist[i] == true) 
-		{
-            int root = find_father(i);
-            cnt[root]++;
-        }
-    }
-
-    int numTrees = 0, numBirds = 0;
-    for(int i = 1; i <= maxn; i++) 
-	{
-        if(exist[i] == true && cnt[i] != 0) 
-		{
-            numTrees++;
-            numBirds += cnt[i];
-        }
-    }
-
-	
-	cin>>m;
-
-    int ida, idb;
-    for(int i = 0; i < m; i++) 
-	{
-		cin>>ida>>idb;
-
-		if(find_father(ida) == find_father(idb))    result.push_back("Yes");
-		else    result.push_back("No");
-    }
-
-
-	cout<<numTrees<<" "<<numBirds<<endl;
-	for(int i=0;i<result.size();i++)
-	{
-		cout<<result[i];
-		if(i!=result.size()-1)    cout<<endl;
+		int b1, b2;
+		cin >> b1 >> b2;
+		cout << (find_root(pic[b1]) == find_root(pic[b2]) ? "Yes" : "No") << endl;
 	}
-
 
     return 0;
 }
